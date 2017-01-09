b6479d86c7f76a092d4a3e8c658c860b79c6fc5c
Linda Juarez <lzjuarez@fullsail.edu>
2017-01-08 22:16:22 -0500

    AI outline

--- Extinguish/Extinguish/AI.cpp
+++ Extinguish/Extinguish/AI.cpp
@@ -1,2 +1,61 @@
 #include "AI.h"
 
+void AI::UpdateState(State newState)
+{
+	switch (currState)
+	{
+	case idle: Idle(); break;
+
+	case getBall: GetBall(); break;
+
+	case attack: Attack(); break;
+
+	case score: Score(); break;
+
+	default: break;
+
+	}
+}
+
+void AI::Update()
+{
+	// check events and UpdateState accordingly
+}
+
+void AI::Idle()
+{
+	// im not sure why they'd be in idle...
+	// maybe something with power-ups
+	// or tracking
+}
+
+void AI::GetBall()
+{
+	// find ball
+
+	// if person (who is enemy) already has it
+		// RunTo(that guy)
+
+	// if person (who is team) has it
+		// if enemies are around him
+			// attack them
+}
+
+void AI::Attack() //
+{
+	// if enemy
+		// hit
+}
+
+void AI::Score()
+{
+	// assuming i have the ball
+
+}
+
+//RunTo(some location?)
+
+//RunTo(gameObject Target)
+	//...
+	// if next to
+		//Attack(Target)
--- Extinguish/Extinguish/AI.h
+++ Extinguish/Extinguish/AI.h
@@ -3,5 +3,25 @@
 
 class AI : Component
 {
+private:
+	enum State
+	{
+		idle,
+		getBall,
+		attack,
+		score,
+	};
+	
 
+	State currState;
+
+
+
+public:
+	void Update();
+	void UpdateState(State newState);
+	void Idle();
+	void GetBall();
+	void Attack();
+	void Score();
 };
