4834bb82eb23a0f671c133da94f793f2c8dc7036
Tom Devaney <Glorified Flashlight>
2017-01-16 09:17:18 -0500

    Changed Translate to addVelocity in movement.

--- Extinguish/Extinguish/Movement.cpp
+++ Extinguish/Extinguish/Movement.cpp
@@ -16,8 +16,8 @@ void Movement::Update(float dt, InputManager* input)
 	if (input->GetKey('I'))
 	{
 		XMFLOAT3 forward = transform->GetForward();
-		transform->AddVelocity({ 0.0f, 0.0f, moveSpeed});
-		transform->Translate({ forward.x * moveSpeed * dt, forward.y * moveSpeed * dt,  forward.z * moveSpeed * dt });
+		//transform->AddVelocity({ 0.0f, 0.0f, moveSpeed});
+		transform->AddVelocity({ forward.x * moveSpeed * dt, forward.y * moveSpeed * dt,  forward.z * moveSpeed * dt });
 		isMoving = true;
 	}
 
@@ -25,40 +25,40 @@ void Movement::Update(float dt, InputManager* input)
 	{
 		//transform->Translate({ 0.0f, 0.0f, -moveSpeed * dt });
 		XMFLOAT3 forward = transform->GetForward();
-		transform->Translate({ forward.x * -moveSpeed * dt, forward.y * -moveSpeed * dt,  forward.z * -moveSpeed * dt });
-		transform->AddVelocity({ 0.0f, 0.0f, -moveSpeed});
+		transform->AddVelocity({ forward.x * -moveSpeed * dt, forward.y * -moveSpeed * dt,  forward.z * -moveSpeed * dt });
+		//transform->AddVelocity({ 0.0f, 0.0f, -moveSpeed});
 		isMoving = true;
 	}
 
 	if (input->GetKey('J'))
 	{
 		XMFLOAT3 right = transform->GetRight();
-		transform->Translate({ right.x * -moveSpeed * dt, right.y * -moveSpeed * dt,  right.z * -moveSpeed * dt });
-		transform->AddVelocity({ -moveSpeed, 0.0f, 0.0f });
+		transform->AddVelocity({ right.x * -moveSpeed * dt, right.y * -moveSpeed * dt,  right.z * -moveSpeed * dt });
+		//transform->AddVelocity({ -moveSpeed, 0.0f, 0.0f });
 		isMoving = true;
 	}
 
 	if (input->GetKey('L'))
 	{
 		XMFLOAT3 right = transform->GetRight();
-		transform->Translate({ right.x * moveSpeed * dt, right.y * moveSpeed * dt,  right.z * moveSpeed * dt });
-		transform->AddVelocity({ moveSpeed, 0.0f, 0.0f });
+		transform->AddVelocity({ right.x * moveSpeed * dt, right.y * moveSpeed * dt,  right.z * moveSpeed * dt });
+		//transform->AddVelocity({ moveSpeed, 0.0f, 0.0f });
 		isMoving = true;
 	}
 
 	if (input->GetKey('U')) //up
 	{
 		XMFLOAT3 up = transform->GetUp();
-		transform->Translate({ up.x * moveSpeed * dt, up.y * moveSpeed * dt,  up.z * moveSpeed * dt });
-		transform->AddVelocity({ 0.0f, moveSpeed, 0.0f });
+		transform->AddVelocity({ up.x * moveSpeed * dt, up.y * moveSpeed * dt,  up.z * moveSpeed * dt });
+		//transform->AddVelocity({ 0.0f, moveSpeed, 0.0f });
 		isMoving = true;
 	}
 
 	if (input->GetKey('P')) //down
 	{
 		XMFLOAT3 up = transform->GetUp();
-		transform->Translate({ up.x * -moveSpeed * dt, up.y * -moveSpeed * dt,  up.z * -moveSpeed * dt });
-		transform->AddVelocity({ 0.0f, -moveSpeed, 0.0f });
+		transform->AddVelocity({ up.x * -moveSpeed * dt, up.y * -moveSpeed * dt,  up.z * -moveSpeed * dt });
+		//transform->AddVelocity({ 0.0f, -moveSpeed, 0.0f });
 		isMoving = true;
 	}
 }
