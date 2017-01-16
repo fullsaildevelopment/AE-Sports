24e5750893fc3d2171424153ac3103ae7f5d5d1c
Linda Juarez <lzjuarez@fullsail.edu>
2017-01-16 12:55:32 -0500

    ball controller stuff

--- Extinguish/Extinguish/AI.cpp
+++ Extinguish/Extinguish/AI.cpp
@@ -125,20 +125,18 @@ void AI::RunTo(GameObject *target, bool isPerson, bool isEnemy)
 	//degRad - degrees/radians between me and target
 	float degRad = (dot_product(u, u) / (u.magnitude() * v.magnitude()));
 
-	//while im not next to target
-	while (v.magnitude() > 5)
-	{
-
-
-		if (isPerson && isEnemy)
-			Attack(target);
-	}
 	me->GetTransform()->RotateX(degRad);
 
 	// run to them
 	me->GetTransform()->AddVelocity(XMFLOAT3(12, 12, 12));
 
-	
+	if (v.magnitude() < 5)
+	{
+		if (isPerson && isEnemy)
+			Attack(target);
+
+		// UpdateState?
+	}
 }
 
 void AI::TossTo(float3 location)
