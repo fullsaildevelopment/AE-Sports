1da3e4ae0387d01ca7347821c80efcef7e628d5f
Nicholas Gilden <nbgilden@fullsail.edu>
2017-01-25 11:10:42 -0500

    scaled plane

--- Extinguish/Extinguish/CollisionLibrary.cpp
+++ Extinguish/Extinguish/CollisionLibrary.cpp
@@ -915,6 +915,10 @@ bool CapsuleToSphereReact(const Capsule& capsule, Sphere& sphere, float3& vel)
 
 bool HexagonToSphere(const Hexagon& hex, Sphere& s, float3& vel)
 {
+	//float3 p2 = 
+
+
+
 	float q2x = abs(s.m_Center.x - hex.seg.m_Start.x);
 	float q2y = abs(s.m_Center.y - hex.seg.m_Start.y);
 	if (q2x > hex.h || q2y > hex.v * 2) return false;
