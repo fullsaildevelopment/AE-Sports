0f2b04b43aeedbbf2345c72a17c9d6104c394327
TomDevaney <thattomdevaney@gmail.com>
2017-01-25 10:08:05 -0500

    Might've forgotten to have added the event files.
    Also, tweaked the way input is handled in event handlers.

---
+++ Extinguish/Bin/EventManager/InputDownEvent.cpp
@@ -0,0 +1,53 @@
+#include "InputDownEvent.h"
+
+//InputDownEvent::InputDownEvent(int key, int mouse)
+//InputDownEvent::InputDownEvent(InputManager* inputManager, bool isServer)
+//{
+//	input = inputManager;
+//	this->isServer = isServer;
+//}
+
+InputDownEvent::InputDownEvent(InputManager* inputManager, int id, bool server)
+{
+	input = inputManager;
+	clientID = id;
+	isServer = server;
+}
+
+InputDownEvent::~InputDownEvent()
+{
+
+}
+
+//no need to implement
+void InputDownEvent::Init()
+{
+
+}
+
+//getters//
+InputManager* InputDownEvent::GetInput()
+{
+	return input;
+}
+
+int InputDownEvent::GetID()
+{
+	return clientID;
+}
+
+bool InputDownEvent::IsServer()
+{
+	return isServer;
+}
+
+//setters//
+void InputDownEvent::SetID(int id)
+{
+	clientID = id;
+}
+
+void InputDownEvent::SetIsServer(bool toggle)
+{
+	isServer = toggle;
+}
---
+++ Extinguish/Bin/EventManager/InputDownEvent.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Event.h"
+//#include "InputManager.h"
+
+class InputManager;
+
+class InputDownEvent : public Event
+{
+private:
+	InputManager* input;
+	int clientID;
+	bool isServer;
+
+
+public:
+	//InputDownEvent(InputManager* inputManager, bool isServer);
+	InputDownEvent(InputManager* inputManager, int id, bool server);
+	~InputDownEvent();
+
+	void Init() override;
+
+	//getters
+	InputManager* GetInput();
+	int GetID();
+	bool IsServer();
+	//bool IsServer();
+
+	//setters
+	void SetIsServer(bool toggle);
+	void SetID(int id);
+};
