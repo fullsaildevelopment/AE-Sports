bd9a1299478ce816f4fb09cb8149edfcd7b04e15
Linda Juarez <lzjuarez@fullsail.edu>
2017-01-27 11:00:19 -0500

    m

--- Extinguish/Extinguish/Game.h
+++ Extinguish/Extinguish/Game.h
@@ -33,8 +33,8 @@ private:
 	ResourceManager resourceManager;
 	SoundEngine soundEngine;
 
-	bool isServer = true;
-	bool isMultiplayer = true;
+	bool isServer = false;
+	bool isMultiplayer = false;
 
 	ClientWrapper client;
 	ServerWrapper server;
