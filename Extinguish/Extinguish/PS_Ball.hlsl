e78470de8da9520afa6ab51f0a5a623feba0833c
Nicholas Gilden <nbgilden@fullsail.edu>
2017-02-09 12:07:54 -0500

    idk

---
+++ Extinguish/Extinguish/PS_Ball.hlsl
@@ -0,0 +1,84 @@
+#define NUMOFPOINTLIGHTS 4
+
+struct PS_BasicInput
+{
+	float4 position : SV_POSITION;
+	float3 normal : NORMAL;
+	float2 uv : TEXCOORD;
+	float4 worldPosition : POSITION0;
+};
+
+cbuffer DirectionalLightCB : register(b0)
+{
+	float4 dirLightNorm;
+	float4 dirLightColor;
+	float4 ambientLight;
+};
+
+struct PointLight
+{
+	float4 pointLightPosition;
+	float4 pointLightColor;
+	float4 lightRadius; //treat as float
+};
+
+cbuffer PointLightCB : register(b1)
+{
+	PointLight pLights[NUMOFPOINTLIGHTS];
+};
+
+cbuffer CamPos : register(b2)
+{
+	float4 camPos;
+};
+
+texture2D baseTexture : register(t0);
+
+SamplerState filter : register(s0);
+
+float4 main(PS_BasicInput input) : SV_TARGET
+{
+	float4 finalColor;
+	float3 diffuseColor, dirColor, pointColor, spotColor;
+	
+	//initialize point and spot colors
+	pointColor = float3(0, 0, 0);
+	spotColor = float3(0, 0, 0);
+	
+	//get texture color with uvs help
+	diffuseColor = (float3)baseTexture.Sample(filter, input.uv);
+	
+	//calculate dircolor
+	float lightRatio;
+	float3 black = { 0.4f, 0.4f, 0.4f };
+	
+	lightRatio = saturate(dot((float3)normalize(dirLightNorm), normalize(input.normal)));
+	dirColor = (lightRatio + (float3)ambientLight) * (float3)dirLightColor * black;
+	
+	//calculate pointlight colors
+	for (int i = 0; i < NUMOFPOINTLIGHTS; ++i)
+	{
+		float pointRatio;
+		float4 pointDir;
+		float pointAttenuation;
+	
+		pointDir = pLights[i].pointLightPosition - input.worldPosition;
+		pointAttenuation = 1 - saturate(length(pointDir) / pLights[i].lightRadius.x);
+		pointRatio = saturate(dot((float3)normalize(pointDir), normalize(input.normal)));
+	
+		pointColor += (float3)pLights[i].pointLightColor * saturate(pointRatio * pointAttenuation) * black;
+	}
+
+
+	//calculate final color
+	finalColor = float4(saturate((dirColor + pointColor + spotColor) * diffuseColor), 1.0f);
+
+	float4 toBall = input.worldPosition - camPos;
+	float doting = dot(normalize(toBall.xyz), input.normal);
+	if (doting >= -1 && doting <= 1)
+	{
+		finalColor = float4(1, 0, 0, 1);
+	}
+
+	return finalColor;
+}
