fb99ba1e1b5402ad14740e747319fa0f305d6272
Nicholas Gilden <nbgilden@fullsail.edu>
2017-03-31 11:02:24 -0400

    fixed jump issue and made some changes to how hexs handle collision

--- Extinguish/Extinguish/CollisionLibrary.cpp
+++ Extinguish/Extinguish/CollisionLibrary.cpp
@@ -1753,14 +1753,14 @@ float3 HexagonToSphere(const Hexagon& hex, Sphere& s, float3& pastPos, float& St
 		{
 			if (planes[0].p.y > endPoint.y && distFromTop.y >= -0.35f)
 			{
-				s.m_Center.y = planes[0].p.y + 0.018f;
+				s.m_Center.y = planes[0].p.y + 0.0018f;
 				Stime = 1;
 				pastPos = s.m_Center;
 				return planes[0].n;
 			}
 			if (planes[0].p.y < endPoint.y && distFromTop.y < 0.06f && Sdirection.y == 0)
 			{
-				s.m_Center.y = planes[0].p.y +  0.018f;
+				s.m_Center.y = planes[0].p.y +  0.0018f;
 				Stime = 1;
 				pastPos = s.m_Center;
 				return planes[0].n;
@@ -1769,7 +1769,7 @@ float3 HexagonToSphere(const Hexagon& hex, Sphere& s, float3& pastPos, float& St
 		if (distFromTop.y <= -0.199f)
 		{
 			float3 revPos = startPoint - endPoint;
-			endPoint = startPoint + revPos * 1.1f;
+			endPoint = startPoint + revPos * 1.15f;
 			s.m_Center = endPoint;
 			return revPos.normalize();
 		}
