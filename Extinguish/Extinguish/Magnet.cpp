b04f7e5af0ff9696dee3affeca08684f9f93b2a2
Tom Devaney <Glorified Flashlight>
2017-04-03 11:26:56 -0400

    Got magnet powerup logic working. Am tweaking the numbers though.

--- Extinguish/Extinguish/Magnet.cpp
+++ Extinguish/Extinguish/Magnet.cpp
@@ -1,18 +1,34 @@
 #include "Magnet.h"
+#include "BallController.h"
+#include "GameObject.h"
 
 //structors
 Magnet::Magnet()
 {
+	SetDuration(8.0f);
+}
 
+void Magnet::Init(GameObject* obj)
+{
+	//cache must be done in init
+	ballC = GetGameObject()->FindGameObject("GameBall")->GetComponent<BallController>();
 }
 
 //basic
 void Magnet::Activate()
 {
-
+	//make magnet range bigger
+	originalMultiplier = ballC->GetMagnetMultiplier();
+	ballC->SetMagnetMultiplier(magnetRangeMultiplier);
+	printf("Magnet activated\n");
 }
 
 void Magnet::Deactivate()
 {
 	PowerUp::Deactivate();
+
+	//make magnet range back to normal
+	ballC->SetMagnetMultiplier(originalMultiplier);
+
+	printf("Magnet deactivated\n");
 }
