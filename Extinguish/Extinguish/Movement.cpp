09c005e35859062d3e66d4f87cce180d1e40eacb
Tom Devaney <Glorified Flashlight>
2017-04-06 13:22:10 -0400

    Added getter and setter for moveSpeed in Movement, so we can slow down the player.

--- Extinguish/Extinguish/Movement.cpp
+++ Extinguish/Extinguish/Movement.cpp
@@ -137,6 +137,12 @@ bool Movement::CanMove()
 	return canMove;
 }
 
+float Movement::GetMoveSpeed()
+{
+	return moveSpeed;
+}
+
+
 //setters//
 void Movement::SetKeys(char forward, char back, char left, char right, char up, char down)
 {
@@ -151,6 +157,11 @@ void Movement::SetKeys(char forward, char back, char left, char right, char up, 
 void Movement::SetCanMove(bool toggle)
 {
 	canMove = toggle;
+}
+
+void Movement::SetMoveSpeed(float speed)
+{
+	moveSpeed = speed;
 }
 
 //private helper functions
